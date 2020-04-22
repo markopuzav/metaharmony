333603ddac6a7838d1cafc8f9eb231cbb53a8894
markopuzav <marko.puza@vacuumlabs.com>
2020-04-22 18:57:12 +0200

    Updates

---
+++ precache-manifest.2ff6156896e81ad85f216be3a99a7bc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bd5bd583edabafdce851fd2ab7f762",
+    "url": "/metaharmony/index.html"
+  },
+  {
+    "revision": "5e4af1a5e7f8712cfbcf",
+    "url": "/metaharmony/static/css/main.81a09027.chunk.css"
+  },
+  {
+    "revision": "de43b5d5d6bf09a1e345",
+    "url": "/metaharmony/static/js/2.43cfbcbf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/metaharmony/static/js/2.43cfbcbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4af1a5e7f8712cfbcf",
+    "url": "/metaharmony/static/js/main.0819a96c.chunk.js"
+  },
+  {
+    "revision": "ff9f22582e6fc4e0600e",
+    "url": "/metaharmony/static/js/runtime-main.117c1629.js"
+  }
+]);
